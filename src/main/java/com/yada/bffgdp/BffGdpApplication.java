66707a1109f67385313a5185aac3393869f7dd1d
Yada dev <yadadev.io@gmail.com>
2022-07-20 21:53:41 -0400

    Init Project

---
+++ src/main/java/com/yada/bffgdp/BffGdpApplication.java
@@ -0,0 +1,13 @@
+package com.yada.bffgdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffGdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffGdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yada/bffgdp/BffGdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yada.bffgdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffGdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
